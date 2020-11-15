1c1b845e95b46bf4f39c51d6712248e67634218b
unknown <Ryan>
2020-11-14 16:00:53 -0800

    added a rick roll in the about command

--- commands/about.js
+++ commands/about.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "get information about this bot",
     execute(message) {
         const exampleEmbed = new Discord.MessageEmbed()
-        .setAuthor('Developed by RA Studios')
+        .setDescription("Developed by [RA Studios](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
         .setFooter('Made with discord.js', 'https://i.imgur.com/wSTFkRM.png');
         message.channel.send(exampleEmbed);
     }
