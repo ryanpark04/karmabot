e06ae1da04138c4c864352828a8c95665b7636ec
Atherna <48332289+Atherna@users.noreply.github.com>
2020-11-15 22:31:08 -0800

    changed pong! to pong

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command",
     execute(message) {
-        message.channel.send('pong!')
+        message.channel.send('pong')
     }
 }
