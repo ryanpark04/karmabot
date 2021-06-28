538241618cac3f635cafd3f6a2e2a9dbe8611dee
Ryan Park <rjpark2004@gmail.com>
2021-06-28 16:49:23 -0700

    changed about page

--- commands/about.js
+++ commands/about.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'get information about this bot',
     execute(message) {
         const exampleEmbed = new Discord.MessageEmbed()
-        .setDescription('Developed by [RA Studios](https://www.youtube.com/watch?v=dQw4w9WgXcQ)')
+        .setDescription('Developed by \*\*ryab#2058\*\*')
         .setFooter('Made with discord.js', 'https://i.imgur.com/wSTFkRM.png');
         message.channel.send(exampleEmbed);
     }
