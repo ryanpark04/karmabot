5d75acb9258f27465a7319f56bf2aae51de53531
ryanpark04 <rjpark2004@gmail.com>
2021-08-16 21:16:42 -0700

    commit from terminal

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
+//test from terminal
+
 let intents = new Discord.Intents(Discord.Intents.NON_PRIVILEGED);
 intents.add('GUILD_MEMBERS')
 
